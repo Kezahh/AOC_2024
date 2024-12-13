206c6b418c2c48bc38d96b6888b3a1b8873df2d7
Kerryn <example@example.com>
2024-12-13 13:47:00 +1100

    day 12 part 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,8 +1,8 @@
 const INPUTS_FOLDER: &str = "inputs/day_12";
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use itertools::sorted;
+use itertools::{sorted, Itertools};
 
 use crate::generic;
 use crate::generic::{Direction, Position};
@@ -38,38 +38,70 @@ impl Region {
         return self.positions.keys().map(|x| self.positions.get(x).unwrap().len()).sum::<usize>();
     }
 
-    fn get_perimeter(&self) -> usize {
+    fn get_perimeter(&self, part_2: bool) -> usize {
         let mut vertical_bars: usize = 0;
+        let mut vertical_bars_p2: usize = 0;
         let mut horizontal_bars: usize = 0;
+        let mut horizontal_bars_p2: usize = 0;
 
         let mut last_col: usize = 0;
-        let mut first_col: bool = true;
+        let mut last_row_set: HashSet<(usize, Direction)> = HashSet::new();
         for row in sorted(self.positions.keys()) {
+            let mut first_col: bool = true;
+            let mut current_row_set: HashSet<(usize, Direction)> = HashSet::new();
             vertical_bars += 2;
             for col in self.positions.get(row).unwrap() {
+                if first_col {
+                    current_row_set.insert((*col, Direction::Down));
+                }
                 if *col > (last_col + 1) && !first_col {
                     vertical_bars += 2;
+                    current_row_set.insert((last_col + 1, Direction::Up));
+                    current_row_set.insert((*col, Direction::Down));
                 }
                 first_col = false;
                 last_col = *col;
             }
+            current_row_set.insert((last_col + 1, Direction::Up));
+            vertical_bars_p2 += last_row_set.difference(&current_row_set).into_iter().collect::<Vec<&(usize, Direction)>>().len();
+            println!("\tRow {}, vertical_bars_p2 = {}, current_row = {:?}", row, vertical_bars_p2, current_row_set);
+            last_row_set = current_row_set;
         }
+        vertical_bars_p2 += last_row_set.len();
+        println!("\tRow end, vertical_bars_p2 = {}, last_row_set = {:?}", vertical_bars_p2, last_row_set);
 
         let mut last_row: usize = 0;
-        let mut first_row: bool = true;
+        let mut last_col_set: HashSet<(usize, Direction)> = HashSet::new();
         for col in sorted(self.positions_col.keys()) {
+            let mut first_row: bool = true;
+            let mut current_col_set: HashSet<(usize, Direction)> = HashSet::new();
             horizontal_bars += 2;
             for row in self.positions_col.get(col).unwrap() {
+                if first_row {
+                    current_col_set.insert((*row, Direction::Right));
+                }
                 if *row > (last_row + 1) && !first_row {
                     horizontal_bars += 2;
+                    current_col_set.insert((last_row + 1, Direction::Left));
+                    current_col_set.insert((*row, Direction::Right));
                 }
                 first_row = false;
                 last_row = *row;
             }
+            current_col_set.insert((last_row + 1, Direction::Left));
+            horizontal_bars_p2 += last_col_set.difference(&current_col_set).into_iter().collect::<Vec<&(usize, Direction)>>().len();
+            println!("\tCol {}, horizontal_bars_p2 = {}, current_col = {:?}", col, horizontal_bars_p2, current_col_set);
+            last_col_set = current_col_set;
         }
+        horizontal_bars_p2 += last_col_set.len();
+        println!("\tCol end, horizontal_bars_p2 = {}, last_col_set = {:?}", horizontal_bars_p2, last_col_set);
         // println!("\tRegion {} has {} vertical and {} horizontal", self.character, vertical_bars, horizontal_bars);
 
-        return vertical_bars + horizontal_bars;
+        if !part_2 {
+            return vertical_bars + horizontal_bars;
+        } else {
+            return vertical_bars_p2 + horizontal_bars_p2;
+        }
     }
 }
 
@@ -157,9 +189,10 @@ fn solve_puzzle(input_filename: String, part_2: bool) -> usize {
     let mut sum_product: usize = 0;
     for r in regions.iter() {
         println!("region {} has area {}", r.character, r.get_area());
-        println!("region {} has perimeter {}", r.character, r.get_perimeter());
+        println!("region {} has perimeter {}", r.character, r.get_perimeter(part_2));
         // println!("\t{:?}", r);
-        sum_product += r.get_area() * r.get_perimeter();
+        sum_product += r.get_area() * r.get_perimeter(part_2);
+        // break;
     }
 
 
@@ -194,6 +227,11 @@ mod tests {
     #[test]
     fn quick_test() {
         // Do a quick test here
+        let s1: HashSet<usize> = HashSet::from_iter([0, 2, 4, 6]);
+        let s2: HashSet<usize> = HashSet::from_iter([0, 2, 5]);
+
+        println!("DIfference len = {:?}", s2.difference(&s1).into_iter().collect::<Vec<&usize>>().len());
+        
     }
 
     #[test]
@@ -214,13 +252,35 @@ mod tests {
     fn example_2() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_1.txt", true);
         println!("Answer = {:?}", answer);
-        assert!(answer == 30);
+        assert!(answer == 1206);
+    }
+
+    #[test]
+    fn example_2_2() {
+        let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_2.txt", true);
+        println!("Answer = {:?}", answer);
+        assert!(answer == 80);
+    }
+
+    #[test]
+    fn example_2_3() {
+        let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_3.txt", true);
+        println!("Answer = {:?}", answer);
+        assert!(answer == 236);
+    }
+
+    #[test]
+    fn example_2_4() {
+        let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_4.txt", true);
+        println!("Answer = {:?}", answer);
+        assert!(answer == 368);
     }
 
     #[test]
     fn part_2() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input.txt", true);
         println!("Answer = {:?}", answer);
-        assert!(answer == 7185540);
+        assert!(answer == 818286);
+        // 814074 too low
     }
 }

5f15170950a8cf85f00f8a3fa9ee6da729cdfefa
Kerryn <example@example.com>
2024-12-18 00:31:21 +1100

    day 17 part 2 wrong answer

--- src/day_17.rs
+++ src/day_17.rs
@@ -78,15 +78,82 @@ impl Instruction {
 
 fn solve_puzzle(input_filename: String, part_2: bool) -> (String, i64, i64, i64) {
     let input_lines: Vec<String> = generic::read_in_file(input_filename.as_str());
-    let mut register_a: i64 = input_lines[0].split(" ").last().unwrap().parse::<i64>().unwrap();
-    let mut register_b: i64 = input_lines[1].split(" ").last().unwrap().parse::<i64>().unwrap();
-    let mut register_c: i64 = input_lines[2].split(" ").last().unwrap().parse::<i64>().unwrap();
+    let register_a_init: i64 = input_lines[0].split(" ").last().unwrap().parse::<i64>().unwrap();
+    let register_b_init: i64 = input_lines[1].split(" ").last().unwrap().parse::<i64>().unwrap();
+    let register_c_init: i64 = input_lines[2].split(" ").last().unwrap().parse::<i64>().unwrap();
+
+    let mut register_a: i64 = register_a_init;
+    let mut register_b: i64 = register_b_init;
+    let mut register_c: i64 = register_c_init;
+
     let program: Vec<usize> = input_lines[4].split(" ").last().unwrap().split(",").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>();
     
-    let mut output: Vec<usize> = Vec::new();
+    let mut output_string: String = String::new();
+    if !part_2 {
+        output_string = do_program(program, &mut register_a, &mut register_b, &mut register_c, part_2);
+        return (output_string, register_a, register_b, register_c);
+    } else {
+        let program_string: String = program.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(",");
+        let mut finished: bool = false;
+        let mut loop_index: usize = 1;
+        let start_offset: i64 = (14 << 22) + (113 << 15) + 3841;
+        println!("Start offset {} bits = {:0b}", start_offset.ilog2() + 1, start_offset);
+        println!("Program string is {:?}", program_string);
+        // let mut loop_index: usize =  35184372000000;
+        let total_loops: usize = 29000000 - 3518400;
+        // let mut loop_index: usize = 290000000000000;
+        // Running loop index 35184663000000 last finished
+        // let mut loop_index: usize = 0;
+        while !finished {
+            let start_a: i64 = (loop_index << 28) as i64 + start_offset;
+            register_a = start_a;
+            register_b = register_b_init;
+            register_c = register_c_init;
+            output_string = do_program(program.clone(), &mut register_a, &mut register_b, &mut register_c, part_2);
+            
+            if output_string == program_string {
+                finished = true;
+                println!("Register A is {}", start_a);
+                //00011000110010011100110011101110001000111100000001 = 2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0
+            }
 
+            if output_string.starts_with("2,4,1,5,7,5,0,3,4,1,1,6,5,5") {
+                println!("{:050b} = {}", (loop_index << 28) as i64 + start_offset, output_string);
+            }
+
+            let mut matches_end: bool = true;
+            for j in 0..output_string.len() {
+                matches_end = matches_end && output_string.chars().nth_back(j) == program_string.chars().nth(j);
+            }
+
+            // if matches_end {
+            //     println!("Loop index = {}, output len = {}, end matches", loop_index, output_string.len());
+            // }
+
+            if loop_index % 1 == 0 {
+                // println!("Running loop index ({}/{})", (loop_index/10000000), total_loops);
+                // println!("{:020b} = {}", (loop_index << 14) as i64 + 3841, output_string);
+                // println!("\tanswer len = {}", output_string.len());
+                // println!("\tprogram_string = {:?}", program_string);
+                // println!("\toutput_string = {:?}", output_string);
+            }
+
+            if loop_index == 1000000000  {
+                break;
+            }
+            loop_index += 1;
+        }
+    }
+
+
+    return (output_string, register_a, register_b, register_c);
+}
+
+fn do_program(program: Vec<usize>, register_a: &mut i64, register_b: &mut i64, register_c: &mut i64, part_2: bool) -> (String) {
+    let mut output: Vec<usize> = Vec::new();
     let mut instruction_index: usize = 0;
     let mut instruction_count: usize = 0;
+    let mut output_matches: Vec<bool> = Vec::new();
 
     while instruction_index < program.len() {
         instruction_count += 1;
@@ -94,45 +161,56 @@ fn solve_puzzle(input_filename: String, part_2: bool) -> (String, i64, i64, i64)
         let current_literal_operand: usize = program[instruction_index + 1];
         let current_combo_operand: i64 = match current_literal_operand {
             0 | 1 | 2 | 3 => current_literal_operand as i64,
-            4 => register_a,
-            5 => register_b,
-            6 => register_c,
+            4 => *register_a,
+            5 => *register_b,
+            6 => *register_c,
             7 => 7,
             _ => panic!("Bad number in program!"),
         };
 
         match current_instruction {
             Instruction::Adv => {
-                register_a = register_a / 2i64.pow(current_combo_operand as u32);
+                *register_a = *register_a / 2i64.pow(current_combo_operand as u32);
             },
             Instruction::Bxl => {
-                register_b = register_b.bitxor(current_literal_operand as i64);
+                *register_b = register_b.bitxor(current_literal_operand as i64);
             },
             Instruction::Bst => {
-                register_b = (current_combo_operand % 8) as i64;
+                *register_b = (current_combo_operand % 8) as i64;
             },
             Instruction::Jnz => {
-                if register_a != 0 {
+                if *register_a != 0 {
                     instruction_index = current_literal_operand;
                 }
             },
             Instruction::Bxc => {
-                register_b = register_b.bitxor(register_c);
+                *register_b = register_b.bitxor(*register_c);
             },
             Instruction::Out => {
                 output.push((current_combo_operand % 8) as usize);
             },
             Instruction::Bdv => {
-                register_b = register_a / 2i64.pow(current_combo_operand as u32);
+                *register_b = *register_a / 2i64.pow(current_combo_operand as u32);
             },
             Instruction::Cdv => {
-                register_c = register_a / 2i64.pow(current_combo_operand as u32);
+                *register_c = *register_a / 2i64.pow(current_combo_operand as u32);
             },
         }
 
-        if !(current_instruction == Instruction::Jnz && register_a != 0) {
+        if !(current_instruction == Instruction::Jnz && *register_a != 0) {
             instruction_index += 2;
         }
+
+        // if output_matches.len() < output.len() {
+        //     for i in output_matches.len()..output.len() {
+        //         if output[i] != program[i] {
+        //             return String::new();
+        //         } else {
+        //             output_matches.push(true);
+        //         }
+        //     }
+        // }
+
         // println!("A = {}, B = {}, C = {}", register_a, register_b, register_c);
 
         // if instruction_count == 10 {
@@ -140,8 +218,7 @@ fn solve_puzzle(input_filename: String, part_2: bool) -> (String, i64, i64, i64)
         // }
     }
 
-
-    return (output.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","), register_a, register_b, register_c);
+    return output.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(",");
 }
 
 
@@ -234,10 +311,11 @@ mod tests {
 
     #[test]
     fn example_2() {
-        let (answer, a, b, c) = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_1.txt", true);
+        let (answer, a, b, c) = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_2.txt", true);
         println!("Answer = {:?}", answer);
         println!("A = {}, B = {}, C = {}", a, b, c);
         assert!(answer == "4,6,3,5,6,3,5,2,1,0");
+        // 117440
     }
 
     #[test]
@@ -246,5 +324,21 @@ mod tests {
         println!("Answer = {:?}", answer);
         println!("A = {}, B = {}, C = {}", a, b, c);
         assert!(answer == "4,6,3,5,6,3,5,2,1,0");
+
+        // 25992398 is too low
+
+
+
+        // got up to 332,680,000
+    }
+
+    #[test]
+    fn part_2_calc() {
+        let loop_index: usize = 25992398;
+        let start_offset: usize = (113 << 15) + 3841;
+        let register_a: usize = (loop_index << 22) + start_offset;
+
+        println!("Register A = {}", register_a);
+        println!("Register A also = {:b}", register_a);
     }
 }

3b81b4b8491a3eb5b058d8347973dac592c2736f
Kerryn <example@example.com>
2024-12-26 18:10:33 +1100

    day 23 part 2

--- src/day_23.rs
+++ src/day_23.rs
@@ -7,7 +7,7 @@ use itertools::Itertools;
 use crate::generic;
 
 
-fn solve_puzzle(input_filename: String, part_2: bool) -> usize {
+fn solve_puzzle(input_filename: String, part_2: bool) -> String {
     let input_lines: Vec<String> = generic::read_in_file(input_filename.as_str());
     let mut computer_map: HashMap<String, HashSet<String>> = HashMap::new();
 
@@ -29,41 +29,72 @@ fn solve_puzzle(input_filename: String, part_2: bool) -> usize {
 
     println!("Map built.");
 
-    let mut connected_computers: HashMap<Vec<String>, bool> = HashMap::new();
-    for (c, c_map) in computer_map.iter() {
-        // println!("{}: {:?}", c, c_map);
-        if c_map.len() >= 2 {
-            for permutation in c_map.iter().cloned().permutations(2) {
-                let mut attempt_connected: Vec<String> = permutation.clone();
-                attempt_connected.push(c.clone());
-                attempt_connected.sort();
-                // println!("Checking {:?}", attempt_connected);
-                if !connected_computers.contains_key(&attempt_connected) {
-                    if computer_map.get(&permutation[0]).unwrap().contains(&permutation[1]) && computer_map.get(&permutation[1]).unwrap().contains(&permutation[0]){
-                        // println!("\t{}: {:?} contains {}", permutation[0], computer_map.get(&permutation[0]).unwrap(), c);
-                        // println!("\t{}: {:?} contains {}", permutation[1], computer_map.get(&permutation[1]).unwrap(), c);
-                        connected_computers.insert(attempt_connected, true);
-                    } else {
-                        connected_computers.insert(attempt_connected, false);
+    if !part_2 {
+
+        let mut connected_computers: HashMap<Vec<String>, bool> = HashMap::new();
+        for (c, c_map) in computer_map.iter() {
+            // println!("{}: {:?}", c, c_map);
+            if c_map.len() >= 2 {
+                for permutation in c_map.iter().cloned().permutations(2) {
+                    let mut attempt_connected: Vec<String> = permutation.clone();
+                    attempt_connected.push(c.clone());
+                    attempt_connected.sort();
+                    // println!("Checking {:?}", attempt_connected);
+                    if !connected_computers.contains_key(&attempt_connected) {
+                        if computer_map.get(&permutation[0]).unwrap().contains(&permutation[1]) && computer_map.get(&permutation[1]).unwrap().contains(&permutation[0]){
+                            // println!("\t{}: {:?} contains {}", permutation[0], computer_map.get(&permutation[0]).unwrap(), c);
+                            // println!("\t{}: {:?} contains {}", permutation[1], computer_map.get(&permutation[1]).unwrap(), c);
+                            connected_computers.insert(attempt_connected, true);
+                        } else {
+                            connected_computers.insert(attempt_connected, false);
+                        }
+                    }
+                }
+            }
+            // break;
+        }
+        
+        let mut sum_t_lans: usize = 0;
+        for (connection, is_connected) in connected_computers {
+            if is_connected {
+                for computer in connection {
+                    if computer.starts_with('t') {
+                        sum_t_lans += 1;
+                        break;
                     }
                 }
             }
         }
-        // break;
+        return sum_t_lans.to_string();
+    } else {
+        let mut all_results: Vec<HashSet<String>> = Vec::new();
+        let mut result: HashSet<String> = HashSet::new();
+        let mut points: HashSet<String> = HashSet::from_iter(computer_map.keys().cloned());
+        let mut excluded: HashSet<String> = HashSet::new();
+        bron_kerbosch(&computer_map, &mut all_results, &mut result, &mut points, &mut excluded);
+        let mut max_result: Vec<&String> = Vec::from_iter(all_results.iter().max_by_key(|x| x.len()).unwrap());
+        max_result.sort();
+        println!("Good result = {:?}", max_result.iter().join(","));
+
+        return max_result.iter().join(",");
     }
-    
-    let mut sum_t_lans: usize = 0;
-    for (connection, is_connected) in connected_computers {
-        if is_connected {
-            for computer in connection {
-                if computer.starts_with('t') {
-                    sum_t_lans += 1;
-                    break;
-                }
-            }
+}
+
+fn bron_kerbosch(computer_map: &HashMap<String, HashSet<String>>, all_results: &mut Vec<HashSet<String>>, result: &mut HashSet<String>, points: &mut HashSet<String>, excluded: &mut HashSet<String>) {
+    if points.is_empty() && excluded.is_empty() {
+        all_results.push(result.clone());
+    } else {
+        while points.len() > 0 {
+            let p: String = points.iter().next().cloned().unwrap();
+            let mut new_result: HashSet<String> = result.clone();
+            let mut good_neighbours: HashSet<String> = HashSet::from_iter(points.intersection(computer_map.get(&p).unwrap()).cloned());
+            let mut new_excluded: HashSet<String> = HashSet::from_iter(points.intersection(&excluded).cloned());
+            new_result.insert(p.clone());
+            bron_kerbosch(computer_map, all_results, &mut new_result, &mut good_neighbours, &mut new_excluded);
+            points.remove(&p);
+            excluded.insert(p);
         }
     }
-    return sum_t_lans;
 }
 
 
@@ -74,33 +105,40 @@ mod tests {
     #[test]
     fn quick_test() {
         // Do a quick test here
+        let l = vec!['a', 'b', 'c', 'd'];
+        for i in 0..l.len() {
+            for j in (i+1)..l.len() {
+                println!("{}{}", l[i],l[j]);
+            }
+        }
     }
 
     #[test]
     fn example_1() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_1.txt", false);
         println!("Answer = {:?}", answer);
-        assert!(answer == 7);
+        assert!(answer == "7");
     }
 
     #[test]
     fn part_1() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input.txt", false);
         println!("Answer = {:?}", answer);
-        assert!(answer == 1215);
+        assert!(answer == "1215");
     }
 
     #[test]
     fn example_2() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_1.txt", true);
         println!("Answer = {:?}", answer);
-        assert!(answer == 30);
+        assert!(answer == "co,de,ka,ta");
     }
 
     #[test]
     fn part_2() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input.txt", true);
         println!("Answer = {:?}", answer);
-        assert!(answer == 7185540);
+        assert!(answer == "bm,by,dv,ep,ia,ja,jb,ks,lv,ol,oy,uz,yt");
+        // "bm,by,dv,ep,ia,ja,jb,ks,lv,ol,oy,uz,yt"
     }
 }

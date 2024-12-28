7b404b6a1211f5a3cca25fd167726ea447f10fb7
Kerryn <example@example.com>
2024-12-28 16:12:40 +1100

    refactor part 1

--- src/day_24.rs
+++ src/day_24.rs
@@ -2,10 +2,11 @@ const INPUTS_FOLDER: &str = "inputs/day_24";
 
 use std::collections::HashMap;
 
-use itertools::Itertools;
+use itertools::{all, Itertools};
 
 use crate::generic;
 
+#[derive(Clone)]
 enum GateType {
     AND,
     OR,
@@ -23,6 +24,7 @@ impl From<&str> for GateType {
     }
 }
 
+#[derive(Clone)]
 struct Gate {
     inputs: Vec<String>,
     output: String,
@@ -62,32 +64,42 @@ fn solve_puzzle(input_filename: String, part_2: bool) -> usize {
     
     println!("{:?}", all_wires);
 
-    while all_gates.len() > 0 {
-        let target_gate: Gate = all_gates.remove(0);
-        if all_wires.contains_key(&target_gate.inputs[0]) && all_wires.contains_key(&target_gate.inputs[1]) {
-            target_gate.calc(&mut all_wires);
+    if !part_2 {
+        all_wires = process_gates(&all_gates, &all_wires);
+        return get_number(&all_wires, 'z');
+    } else {
+        return 0;
+    }
+}
+
+fn process_gates(all_gates: &Vec<Gate>, all_wires: &HashMap<String, usize>) -> HashMap<String, usize> {
+    let mut gates: Vec<Gate> = all_gates.clone();
+    let mut wires: HashMap<String, usize> = all_wires.clone();
+
+    while gates.len() > 0 {
+        let target_gate: Gate = gates.remove(0);
+        if wires.contains_key(&target_gate.inputs[0]) && wires.contains_key(&target_gate.inputs[1]) {
+            target_gate.calc(&mut wires);
         } else {
-            all_gates.push(target_gate);
+            gates.push(target_gate);
         }
     }
 
-    println!("{:?}", all_wires);
+    return wires;
+} 
 
-    let mut z_wires: Vec<usize> = Vec::new();
-    let mut z_number: usize = 0;
-    let mut z_count: usize = 0;
+fn get_number(all_wires: &HashMap<String, usize>, target_char: char) -> usize {
+    let mut number: usize = 0;
+    let mut count: usize = 0;
     for wire_name in all_wires.keys().sorted() {
-        if wire_name.starts_with("z") {
+        if wire_name.starts_with(target_char) {
             let current_z: usize = *all_wires.get(wire_name).unwrap();
-            z_wires.push(current_z);
-            z_number += current_z << z_count;
-            z_count += 1;
+            number += current_z << count;
+            count += 1;
         }
     }
 
-    println!("{:?}", z_wires);
-    println!("{}", z_number);
-    return z_number;
+    return number;
 }
 
 
@@ -98,8 +110,16 @@ mod tests {
     #[test]
     fn quick_test() {
         // Do a quick test here
+        let mut new_vec: Vec<usize> = Vec::new();
+        for i in 0..222 {
+            new_vec.push(i);
+        }
 
-        println!("{}", false as usize);
+        // let mut permutations: Vec<Vec<&usize>> = new_vec.iter().permutations(8).collect::<Vec<Vec<&usize>>>();
+        for (i, p) in new_vec.iter().permutations(8).enumerate() {
+            println!("{:9}{:?}", i, p);
+        }
+        // println!("Len = {}", permutations.len());
     }
 
     #[test]
